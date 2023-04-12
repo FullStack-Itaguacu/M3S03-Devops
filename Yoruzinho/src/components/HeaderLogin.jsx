7c7afa9217d91f17fc9d48b1513ed67952916169
Luis Federico Vilar Velazquez <luis_velazquez@estudante.sesisenai.org.br>
2023-04-12 00:05:46 -0300

    Featt : criacao da interface de Login
    Add :
         -FromLogin.jsx
         -HeaderLogin.jsx
    Config :
         -main.jsx  : import Bootstrap
    Moddified :
         -Login.jsx  : adicionei os componentes da page

---
+++ Yoruzinho/src/components/HeaderLogin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar, Container } from 'react-bootstrap'
+import {MdLocalPharmacy} from 'react-icons/md'
+
+const HeaderLogin = () => {
+  return (
+    <Navbar  bg="dark" variant="dark" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home"><MdLocalPharmacy/>   LABPharmacy Inc.</Navbar.Brand>
+    </Container>
+  </Navbar>
+  )
+}
+
+export default HeaderLogin
--- Yoruzinho/src/main.jsx
+++ Yoruzinho/src/main.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
