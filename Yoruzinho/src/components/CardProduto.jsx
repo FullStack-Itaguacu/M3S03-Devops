1abf3786bc9831178d4b205daccd0385693d69f8
Luis Federico Vilar Velazquez <luis_velazquez@estudante.sesisenai.org.br>
2023-04-17 15:42:15 -0300

    Featt : Modal Produtos
    Add:
         - ModalProduto.jsx | modal  quando chamado mostra todas as caracteristicas do produto
     Fix:
         - Input do valor do produto agora aceita decimais   add prop : step="0.01"
    Config :
         - CardProduto.jsx para renderizar Modal

--- Yoruzinho/src/components/CardProduto.jsx
+++ Yoruzinho/src/components/CardProduto.jsx
@@ -1,54 +1,51 @@
-import React from 'react'
-import { Card, Col } from 'react-bootstrap';
+import React from "react";
+import { Card, Col, Row, Button } from "react-bootstrap";
+import ModalProduto from "./ModalProduto";
 
-const CardProduto = ({medicamento, laboratorio, dosagem, valorUnitario, tipo, descricao='', imagem}) => {
-    const produto = {
-        medicamento:medicamento,
-        laboratorio:laboratorio,
-        dosagem: dosagem,
-        valorUnitario: valorUnitario,
-        tipo: tipo,
-        descricao: descricao,
-        imagem: imagem,
-      };
+const CardProduto = ({
+  medicamento,
+  laboratorio,
+  dosagem,
+  valorUnitario,
+  tipo,
+  descricao = "",
+  imagem,
+}) => {
+  const produto = {
+    medicamento: medicamento,
+    laboratorio: laboratorio,
+    dosagem: dosagem,
+    valorUnitario: valorUnitario,
+    tipo: tipo,
+    descricao: descricao,
+    imagem: imagem,
+  };
   return (
     <>
-        <Card as={Col} md="3">
-            <Card.Img variant="top" src={produto.imagem} />
-            <Card.Body>
-                <Card.Title>{produto.medicamento}</Card.Title>
-                <Card.Text>
-                    {produto.descricao}
-                </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-                <small className="text-muted">R$ {produto.valorUnitario}</small>
-            </Card.Footer>
-        </Card>
-
+      <Card as={Col} md="3">
+        <Card.Img variant="top" src={produto.imagem} />
+        <Card.Body>
+          <Card.Title>{`${produto.medicamento} ${produto.dosagem} ${produto.laboratorio}`}</Card.Title>
+        </Card.Body>
+        <Row>
+    
+          <Button variant="ligth" size="sm" disabled>
+            Valor Unitario R$ {produto.valorUnitario}
+          </Button>
+          <ModalProduto
+            as={Col}
+            descricao={produto.descricao}
+            medicamento={produto.medicamento}
+            dosagem={produto.dosagem}
+            laboratorio={produto.laboratorio}
+            valorUnitario={produto.valorUnitario}
+            tipo={produto.tipo}
+            imagem={produto.imagem}
+          />
+        </Row>
+      </Card>
     </>
-  )
+  );
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-export default CardProduto
+export default CardProduto;
---
+++ Yoruzinho/src/components/ModalProduto.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+const ModalProduto = ({
+  medicamento,
+  laboratorio,
+  dosagem,
+  valorUnitario,
+  tipo,
+  descricao = "",
+}) => {
+  const [show, setShow] = useState(false);
+
+  function handleShow() {
+    setShow(true);
+  }
+
+  return (
+    <>
+        <Button variant="secondary" onClick={() => handleShow()}>
+          Ver mais
+        </Button>
+
+      <Modal show={show} fullscreen={false} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>{medicamento}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{`Descriçao : ${descricao}`}</Modal.Body>
+        <Modal.Body>{`Laboratorio : ${laboratorio}`}</Modal.Body>
+        <Modal.Body>{`Dosagem : ${dosagem}`}</Modal.Body>
+        <Modal.Body>{`Valor Unitario : R$ ${valorUnitario}`}</Modal.Body>
+        <Modal.Body>{`Tipo : ${tipo}`}</Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalProduto;
